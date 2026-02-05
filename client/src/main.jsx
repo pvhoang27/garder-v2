2ffd78660eedf365a5003570a8578e2834425fc4
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-05 15:28:29 +0700

    fix view

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './i18n'; // <--- Thêm dòng này
+import './i18n'; 
 
+// Đã bỏ <StrictMode> để tránh việc useEffect chạy 2 lần (gây lỗi x2 view)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
