a8bb1cdacb47829cd90cdff782ccd1afe2f64c1c
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-02-12 10:09:28 +0700

    Update API URL to production domain

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:3000/api', // Link Backend của bạn
+    baseURL: 'http://caycanhxuanthuc.com/api', // Link Backend của bạn
     withCredentials: true, // [QUAN TRỌNG] Cho phép gửi cookie kèm request
     headers: {
         'Content-Type': 'application/json',
